996153fe6952bfbb0627e0b4d1f1b54779a1b43e
AKASH S <akashsofficial03@gmail.com>
2025-05-29 18:39:38 +0530

    Update shortener.py

--- backend/shortener.py
+++ backend/shortener.py
@@ -6,4 +6,4 @@ url_map = {}  # Dictionary to store short -> original mapping
 def shorten_url(original_url):
     short = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
     url_map[short] = original_url
-    return f"http://your-domain/{short}"  # Replace with actual domain after deployment
+    return f"http://localhost:5000/{short}"# Replace with actual domain after deployment
